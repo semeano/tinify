ce8b9c4b77089962d91eb6381437eed171419214
Pedro Semeano <semeano@gmail.com>
2023-11-22 12:06:17 +0000

    Scale images; Added support for jpeg

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 const { series, parallel, src, dest } = require("gulp");
 const clean = require("gulp-clean");
 const smushit = require("gulp-smushit");
+const flatMap = require("flat-map").default;
+const scaleImages = require("gulp-scale-images");
 
 function cleanOutput() {
   return src("output", { allowEmpty: true }).pipe(clean({ force: true }));
 }
 
 function images(cb) {
-  src("input/*.{jpg,png}")
+  src("input/*.{jpeg, jpg, png}")
+    .pipe(
+      flatMap((file, cb) => {
+        file.scale = { maxHeight: 1200 };
+        cb(null, [file]);
+      })
+    )
+    .pipe(scaleImages())
     .pipe(smushit({ verbose: true }))
     .pipe(dest("output"));
   cb();
